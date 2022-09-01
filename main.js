e8941c96900ef2511e9b128f456ba185e0896ccc
MiniGamerZ <86099945+MiniGamerZ@users.noreply.github.com>
2022-09-01 20:11:33 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,90 @@
+song1 = ""
+song2 = ""
+
+function setup(){
+    canvas = createCanvas(600,500);
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose',gotPoses);
+}
+function modelLoaded(){
+    console.log("PoseNet Is Intaliazed");
+}
+function preload(){
+    song = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+
+ 
+scoreRightWrist = 0;
+scoreLeftWrist = 0;
+
+rightWristX = 0;
+rightWristY = 0;
+
+leftWristX = 0;
+leftWristY = 0;
+
+function gotPoses(results){
+    if(results > 0){
+        console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("score" + scoreRightWrist + scoreLeftWrist);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("right Wrist:" + RightWristX + RightWriteY);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("left wrist" + leftWristX + leftWristY);
+
+    }
+}
+
+    song1status =""
+    song2status = ""
+
+function draw(){
+    image(video, 0, 0, 600, 500);
+
+    fill("#FF0000");
+    stroke("#FF0000");
+
+    song1status = song1.isPlaying();
+    song2status = song2.isPlaying();
+
+    function draw(){
+        image(video, 0, 0, 600, 500);
+    
+        fill("#FF0000");
+        stroke("#FF0000");
+    
+        if(scoreLeftWrist > 0.2)
+        {
+            circle(leftWristX,leftWristY,20);
+            song2.stop;
+            if(song1status == "false"){
+                song1.play;
+                document.getElementById("song").innerHTML = "playing HARRY POTTER THEME SONG";
+            }
+        }
+        if(scoreRightWrist > 0.2)
+        {
+            circle(leftWristX,leftWristY,20);
+            song1.stop;
+            if(song2status == "false"){
+                song2.play;
+                document.getElementById("song").innerHTML = "playing PETER PAN THEME SONG";
+            }
+        }
+    }
+
+
+}
